8de1af2df5dd495f4f646a94ef00061c09e12db6
Yana <ya_yana@ngs.ru>
2022-04-08 14:26:55 +0700

    8. Все поля заполнены.

--- src/test/java/guru/qa/PracticeFormTest.java
+++ src/test/java/guru/qa/PracticeFormTest.java
@@ -5,20 +5,29 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class PracticeFormTest {
 
     @BeforeAll
-    static void setUp(){
+    static void setUp() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = ("1920x1080");
+       /* System.setProperty("webdriver.chrome.driver", "C:/Users/User/IdeaProjects/first-test/build/downloads/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverRunner.setWebDriver(driver);*/
+
     }
 
     @Test
@@ -31,17 +40,25 @@ public class PracticeFormTest {
         $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("9130000000");
         $("#subjectsInput").setValue("English").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("December");
         $(".react-datepicker__year-select").selectOption("2022");
         $("[aria-label$='December 2nd, 2022']").click();
+        $x("//input[@type='file']").uploadFile(new File("C:/Users/User/IdeaProjects/first-test/src/resources/20.jpg"));
         $("[id= currentAddress]").setValue("AnyWhere...");
-
+        $("[id=state]").click();
+        $("[id=stateCity-wrapper]").$(byText("NCR")).click();
+        $("[id=city]").click();
+        $("[id=stateCity-wrapper]").$(byText("Delhi")).click();
         $("#submit").click();
 
         $(".table-responsive").shouldHave(text("Yana"), text("Muntyanova"), text("Muntyanova@ya.ru"),
                 text("Male"), text("9130000000"),text("02 December,2022"), text("English"),text("AnyWhere..."));
-    }
+
+
+
+       }
 
 
 
