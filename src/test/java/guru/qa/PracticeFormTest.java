7e66062202c2a3ca875a20a3fc77cbceb27981ab
Yana <ya_yana@ngs.ru>
2022-04-14 01:14:30 +0700

    11.  - Исправлены названия методов, класса. Переменные убраны в  class PracticeFormTest, все вводные данные убраны в переменные.

--- src/test/java/guru/qa/PracticeFormTest.java
+++ src/test/java/guru/qa/PracticeFormTest.java
@@ -4,47 +4,62 @@ import com.codeborne.selenide.*;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class PracticeFormTest {
 
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName(),
+           lastName = faker.name().lastName(),
+           userEmail = faker.internet().emailAddress(),
+            userNumber = faker.numerify("##########"),
+            currentAddress =faker.address().fullAddress(),
+            gender = faker.demographic().sex(),
+            subjects = "English",
+            hobbies = "Reading",
+            day = "02",
+            month = "December",
+            year = "2022",
+            state = "NCR",
+            city = "Delhi";
 
     @BeforeAll
     static void setUp() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = ("1920x1080");
-      /*  System.setProperty("webdriver.chrome.driver", "C:/Users/User/IdeaProjects/first-test/build/downloads/chromedriver.exe");
+      //  Configuration.browserSize = ("1920x1080");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/User/IdeaProjects/first-test/build/downloads/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        WebDriverRunner.setWebDriver(driver);*/
-
+        WebDriverRunner.setWebDriver(driver);
     }
+
 
     @Test
     void fillFromTest(){
 
-        RegistrationFormTest  registrationFormTest = new RegistrationFormTest();
+        RegistrationFormPage  registrationFormPage = new RegistrationFormPage();
 
-        registrationFormTest.openPage();
-        registrationFormTest.setFirstName();
-        registrationFormTest.setLastNamee();
-        registrationFormTest.setUserEmail();
-        registrationFormTest.setUserNumber();
-        registrationFormTest.setCurrentAddress();
-        registrationFormTest.setGender();
-        registrationFormTest.setSubjects();
-        registrationFormTest.setHobbiesWrapper();
-        registrationFormTest.setDateOfBirth();
-        registrationFormTest.setFileinput();
-        registrationFormTest.setState();
+        registrationFormPage.openPage();
+        registrationFormPage.setFirstName(firstName);
+        registrationFormPage.setLastNamee(lastName);
 
-        registrationFormTest.setSubmit();
+        registrationFormPage.setUserEmail(userEmail);
+        registrationFormPage.setUserNumber(userNumber);
+        registrationFormPage.setCurrentAddress(currentAddress);
+        registrationFormPage.setGender(gender);
+        registrationFormPage.setSubjects(subjects);
+        registrationFormPage.setHobbiesWrapper(hobbies);
+        registrationFormPage.setDateOfBirth(month, year, day);
+        registrationFormPage.setFileinput();
+        registrationFormPage.setState(state, city);
 
-        registrationFormTest.setAssert();
+        registrationFormPage.setButtonClick();
+
+        registrationFormPage.setCheckResult(firstName, lastName, userEmail, gender,  month, year, day, userNumber, hobbies, currentAddress);
 
 
        }
---
+++ src/test/java/guru/qa/RegistrationFormPage.java
@@ -0,0 +1,53 @@
+package guru.qa;
+import com.codeborne.selenide.Condition;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class RegistrationFormPage {
+
+
+    public void openPage() {
+        open("/automation-practice-form");
+    }
+    public void setFirstName(String firstName) { $("[id=firstName]").setValue(firstName);}
+    public void setLastNamee(String lastName) {  $("[id=lastName]").setValue(lastName); }
+    public void setUserEmail(String userEmail) {  $("[id=userEmail]").setValue(userEmail); }
+    public void setUserNumber(String userNumber) {  $("[id=userNumber]").setValue(userNumber); }
+    public void setCurrentAddress(String currentAddress) {  $("[id=currentAddress]").setValue(currentAddress); }
+    public void setGender(String gender) { $("#genterWrapper").$(byText(gender)).click(); }
+    public void setSubjects(String subjects) {$("#subjectsInput").setValue(subjects).pressEnter(); }
+    public void setHobbiesWrapper(String hobbies) { $("#hobbiesWrapper").$(byText(hobbies)).click();}
+
+    public void setDateOfBirth(String month, String year, String day) {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0" + day +
+                ":not(.react-datepicker__day--outside-month)").click();
+      //  $("[aria-label$='December 2nd, 2022']").click();
+    }
+    public void setFileinput() {$x("//input[@type='file']").uploadFile(new File("src/test/resources/20.jpg"));}
+    public void setState(String state, String city) {
+        $("[id=state]").click();
+        $("[id=stateCity-wrapper]").$(byText(state)).click();
+        $("[id=city]").click();
+        $("[id=stateCity-wrapper]").$(byText(city)).click();
+    }
+
+    public void setButtonClick() { $("#submit").click();}
+
+    public void setCheckResult(String firstName, String lastName, String userEmail, String gender, String month, String  year, String day,
+                               String userNumber, String hobbies, String currentAddress)
+    {
+        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(userEmail),
+        text(gender), text(String.valueOf(userNumber)),text(day + " "+ month + "," + year), text(hobbies),text(currentAddress));
+    }
+
+
+
+}
