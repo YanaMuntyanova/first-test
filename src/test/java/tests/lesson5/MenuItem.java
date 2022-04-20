4541354e60d41809569d925761bfd20e0094b139
Yana <ya_yana@ngs.ru>
2022-04-20 14:10:29 +0700

    12.  - Датапровайдеры @CsvSource, @ValueSource, MethodSource, EnumSource

---
+++ src/test/java/tests/lesson5/MenuItem.java
@@ -0,0 +1,10 @@
+package tests.lesson5;
+
+public enum MenuItem {
+    News("News"), Entertainment("Entertainment");
+    public final String rusName;
+
+    MenuItem(String rusName) {
+        this.rusName = rusName;
+    }
+}
